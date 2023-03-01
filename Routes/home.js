fb93f87116b511fdc18d7206e8733ee980dd462e
Abhishek Upadhyay <megablazikenabhishek@gmail.com>
2023-03-01 21:33:02 +0530

    bawal

--- Routes/home.js
+++ Routes/home.js
@@ -1,12 +1,46 @@
 const express = require("express");
 const router = express.Router();
+const uploader = require("express-fileupload");
+const cloudinary = require("../config/cloudinary");
+const Item = require("../models/Items");
 
 router.get("/uploadItem", (req, res)=>{
     res.render("upload");
 })
 
-router.post("/uploadItem", (req, res)=>{
-    res.send("hii");
+router.post("/uploadItem", uploader({useTempFiles:true}) , async(req, res)=>{
+    let product = {
+        product_name : req.body.name,
+        base_price : req.body.base_price,
+        time_stamp: new Date().setDate(new Date().getDate+Number(req.body.time_stamp)),
+        location: req.body.location,
+        details: req.body.details,
+        photos: []
+    };
+
+    if(req.files.photos.tempFilePath){
+        await cloudinary.uploader.upload(req.files.photos.tempFilePath, (err, result)=>{
+            product.photos.push(result.url);
+        }).catch(err=>console.log(err))
+    }
+    else{
+        // console.log(req.files.photos)
+        for(let j=0; j<req.files.photos.length; j++){
+            let i = req.files.photos[j];
+            await cloudinary.uploader.upload(i.tempFilePath, (err, result)=>{
+                product.photos.push(result.url);
+            }).catch(err=>console.log(err))
+        }
+    }
+    // console.log(product);
+    Item.create(product)
+        .then(res=>console.log(res));
+    require("rimraf")("tmp");
+    res.send({msg:"done"});
 })
 
+router.post("/testing",uploader({useTempFiles:true}), (req, res)=>{
+    console.log(req.files);
+    res.redirect("/test1.html");
+})
 module.exports = router;
