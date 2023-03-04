b14350be832354178e6a9698a4f78aa8c1e6228e
Abhishek Upadhyay <megablazikenabhishek@gmail.com>
2023-03-04 16:49:46 +0530

    get

--- routes/landing.js
+++ routes/landing.js
@@ -6,7 +6,7 @@ const User = require("../models/user");
 
 const {isAuth} = require("../middlewares/authorizationMiddleware");
 
-router.route("/", (req, res)=>{
+router.get("/", (req, res)=>{
     res.redirect("/login");
 })
 
