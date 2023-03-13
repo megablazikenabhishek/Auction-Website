ae46a67ea9d01a9918bf2f16d7b381bd319acf8b
Restyled.io <commits@restyled.io>
2023-03-13 14:44:11 +0000

    Restyled by prettier

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -1,63 +1,67 @@
-const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+const allSideMenu = document.querySelectorAll("#sidebar .side-menu.top li a");
 
-allSideMenu.forEach(item => {
+allSideMenu.forEach((item) => {
   const li = item.parentElement;
 
-  item.addEventListener('click', function() {
-    allSideMenu.forEach(i => { i.parentElement.classList.remove('active'); })
-    li.classList.add('active');
-  })
+  item.addEventListener("click", function () {
+    allSideMenu.forEach((i) => {
+      i.parentElement.classList.remove("active");
+    });
+    li.classList.add("active");
+  });
 });
 
 // TOGGLE SIDEBAR
-const menuBar = document.querySelector('#content nav .bx.bx-menu');
-const sidebar = document.getElementById('sidebar');
+const menuBar = document.querySelector("#content nav .bx.bx-menu");
+const sidebar = document.getElementById("sidebar");
 
-menuBar.addEventListener('click',
-                         function() { sidebar.classList.toggle('hide'); })
+menuBar.addEventListener("click", function () {
+  sidebar.classList.toggle("hide");
+});
 
-const searchButton =
-    document.querySelector('#content nav form .form-input button');
-const searchButtonIcon =
-    document.querySelector('#content nav form .form-input button .bx');
-const searchForm = document.querySelector('#content nav form');
+const searchButton = document.querySelector(
+  "#content nav form .form-input button"
+);
+const searchButtonIcon = document.querySelector(
+  "#content nav form .form-input button .bx"
+);
+const searchForm = document.querySelector("#content nav form");
 
-searchButton.addEventListener('click', function(e) {
+searchButton.addEventListener("click", function (e) {
   if (window.innerWidth < 576) {
     e.preventDefault();
-    searchForm.classList.toggle('show');
-    if (searchForm.classList.contains('show')) {
-      searchButtonIcon.classList.replace('bx-search', 'bx-x');
+    searchForm.classList.toggle("show");
+    if (searchForm.classList.contains("show")) {
+      searchButtonIcon.classList.replace("bx-search", "bx-x");
     } else {
-      searchButtonIcon.classList.replace('bx-x', 'bx-search');
+      searchButtonIcon.classList.replace("bx-x", "bx-search");
     }
   }
-})
+});
 
 if (window.innerWidth < 768) {
-  sidebar.classList.add('hide');
-}
-else if (window.innerWidth > 576) {
-  searchButtonIcon.classList.replace('bx-x', 'bx-search');
-  searchForm.classList.remove('show');
+  sidebar.classList.add("hide");
+} else if (window.innerWidth > 576) {
+  searchButtonIcon.classList.replace("bx-x", "bx-search");
+  searchForm.classList.remove("show");
 }
 
-window.addEventListener('resize', function() {
+window.addEventListener("resize", function () {
   if (this.innerWidth > 576) {
-    searchButtonIcon.classList.replace('bx-x', 'bx-search');
-    searchForm.classList.remove('show');
+    searchButtonIcon.classList.replace("bx-x", "bx-search");
+    searchForm.classList.remove("show");
   }
-})
+});
 
-const switchMode = document.getElementById('switch-mode');
+const switchMode = document.getElementById("switch-mode");
 
-switchMode.addEventListener('change', function() {
+switchMode.addEventListener("change", function () {
   if (this.checked) {
-    document.body.classList.add('dark');
+    document.body.classList.add("dark");
   } else {
-    document.body.classList.remove('dark');
+    document.body.classList.remove("dark");
   }
-})
+});
 
 const render = async () => {
   try {
@@ -131,7 +135,9 @@ const render = async () => {
   }
 };
 
-window.addEventListener("load", () => { render(); });
+window.addEventListener("load", () => {
+  render();
+});
 
 // https://res.cloudinary.com/dyszi81jo/image/upload/v1677738575/ivdvvksrd3yfvuprqhkj.jpg
 // https://auction-website-o61d.onrender.com/home/getItems
