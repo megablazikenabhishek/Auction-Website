31158cd20de245dea86ea36a7a6438f6560ca5ed
Abhishek Upadhyay <megablazikenabhishek@gmail.com>
2023-03-18 15:01:58 +0530

    hummmmm

--- config/cron_job.js
+++ config/cron_job.js
@@ -1,5 +1,8 @@
 const cron = require("node-cron");
 const Item = require("../models/Items");
+const getHtml = require("../lib/generatePDF");
+const User = require("../models/user");
+const nodemailer = require("nodemailer");
 // declaring the winner is remaining yet
 
 cron.schedule("0 0 * * *", async()=>{
@@ -25,20 +28,51 @@ cron.schedule("0 0 * * *", async()=>{
     }
 })
 
-cron.schedule('0 1 * * *', async() => {
+// cron.schedule('0 1 * * *', async() => {
   // code to run at 1 am every day
+const check = async()=>{
     console.log("Running Cron Job for Winners.................");
     try {
         let result = await Item.find({sold:false, expired: true})
             .select("current_bid product_name details photos time_stamp winner");
 
-        result.map(async i=>{
+        const sendMail = [];
+        for(let it = 0; it<result.length; it++){
+            const i = result[it];
             if(i.current_bid.name !== "-none-"){
-                await Item.findOneAndUpdate(i._id, {winner:{name:i.current_bid.name, _id:i.current_bid.user_id, amount: i.current_bid.amount}})
+                await Item.findOneAndUpdate(i._id, { sold:true,winner:{name:i.current_bid.name, _id:i.current_bid.user_id, amount: i.current_bid.amount}})
                 .catch(err=>console.log(err))
+
+                const user = await User.findById(i.current_bid.user_id);
+                sendMail.push({name: i.current_bid.name, product_name: i.product_name, price: i.current_bid.amount, time_stamp: i.time_stamp, email: user.email})
             }
-        })
-    } catch (error) {
+        }
+
+        // console.log(sendMail);
+        var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASS
+        }
+        });
+        sendMail.forEach(async i => {
+            let mailOptions = {
+                from: process.env.EMAIL,
+                to: i.email,
+                subject: 'Certify Email',
+                html: getHtml(i.name, i.product_name, i.price, i.time_stamp),
+            };
         
+            await transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                    console.log(error);
+                }
+            });
+        });
+    } catch (error) {
+        console.log(error);
     }
-});
+}
+check();
+// });
