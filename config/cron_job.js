a067a83b9462d7d2081af1d23b0d038b224cfec8
Restyled.io <commits@restyled.io>
2023-03-22 09:22:25 +0000

    Restyled by clang-format

--- config/cron_job.js
+++ config/cron_job.js
@@ -4,77 +4,91 @@ const getHtml = require("../lib/generatePDF");
 const User = require("../models/user");
 const nodemailer = require("nodemailer");
 var pdf = require('html-pdf');
-var options = { format: 'Letter' };
+var options = {format : 'Letter'};
 const format = require("date-format")
 // declaring the winner is remaining yet
 
 cron.schedule("*/30 * * * *", async () => {
-    console.log("Running Cron Job for update........");
-    try {
-        let result = await Item.find({ sold: false, expired: false })
+  console.log("Running Cron Job for update........");
+  try {
+    let result =
+        await Item.find({sold : false, expired : false})
             .select("current_bid product_name details photos time_stamp")
-        result.map(async i => {
-            const date = new Date();
-            if (i.time_stamp <= date && i.current_bid.name !== "-none-") {
-                await Item.findOneAndUpdate(i._id, { expired: true, sold: true })
-                    .catch(err => console.log(err))
-            }
-            else if (i.time_stamp <= date) {
-                await Item.findOneAndUpdate(i._id, { expired: true })
-                    .catch(err => console.log(err))
-            } else
-                return i;
-        })
-        // console.log(result);
-    } catch (err) {
-        console.log(err);
-    }
+    result.map(async i => {
+      const date = new Date();
+      if (i.time_stamp <= date && i.current_bid.name !== "-none-") {
+        await Item.findOneAndUpdate(i._id, {expired : true, sold : true})
+            .catch(err => console.log(err))
+      } else if (i.time_stamp <= date) {
+        await Item.findOneAndUpdate(i._id, {expired : true})
+            .catch(err => console.log(err))
+      } else
+        return i;
+    })
+    // console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
 })
 cron.schedule('*/35 * * * *', async () => {
-// const check = async()=>{
-    console.log("Running Cron Job for Winners.................");
-    try {
-        let result = await Item.find({ sold: true, expired: true, sent: false })
-            .select("current_bid product_name details photos time_stamp winner");
+  // const check = async()=>{
+  console.log("Running Cron Job for Winners.................");
+  try {
+    let result =
+        await Item.find({sold : true, expired : true, sent : false})
+            .select(
+                "current_bid product_name details photos time_stamp winner");
 
-        const sendMail = [];
-        for (let it = 0; it < result.length; it++) {
-            const i = result[it];
-            if (i.current_bid.name !== "-none-") {
-                await Item.findOneAndUpdate(i._id, { sold: true, sent: true, winner: { name: i.current_bid.name, _id: i.current_bid.user_id, amount: i.current_bid.amount } })
-                    .catch(err => console.log(err))
+    const sendMail = [];
+    for (let it = 0; it < result.length; it++) {
+      const i = result[it];
+      if (i.current_bid.name !== "-none-") {
+        await Item
+            .findOneAndUpdate(i._id, {
+              sold : true,
+              sent : true,
+              winner : {
+                name : i.current_bid.name,
+                _id : i.current_bid.user_id,
+                amount : i.current_bid.amount
+              }
+            })
+            .catch(err => console.log(err))
 
-                const user = await User.findById(i.current_bid.user_id);
-                sendMail.push({ name: i.current_bid.name, product_name: i.product_name, price: i.current_bid.amount, time_stamp: format.asString("dd/MM/yyyy", new Date(i.time_stamp)), email: user.email })
-            }
-        }
-
-        // console.log(sendMail);
-        var transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.PASS
-            }
-        });
-        await sendMail.forEach(async i => {
-            // creating pdf
-            let mailOptions = {
-                from: process.env.EMAIL,
-                to: i.email,
-                subject: 'Certify Email',
-                html: getHtml(i.name, i.product_name, i.price, i.time_stamp),
-            };
-
-            await transporter.sendMail(mailOptions, function (error, info) {
-                if (error) {
-                    return console.log(error);
-                }
-            });
-        });
-    } catch (error) {
-        console.log(error);
+        const user = await User.findById(i.current_bid.user_id);
+        sendMail.push({
+          name : i.current_bid.name,
+          product_name : i.product_name,
+          price : i.current_bid.amount,
+          time_stamp : format.asString("dd/MM/yyyy", new Date(i.time_stamp)),
+          email : user.email
+        })
+      }
     }
-    // }
-    // check();
+
+    // console.log(sendMail);
+    var transporter = nodemailer.createTransport({
+      service : 'gmail',
+      auth : {user : process.env.EMAIL, pass : process.env.PASS}
+    });
+    await sendMail.forEach(async i => {
+      // creating pdf
+      let mailOptions = {
+        from : process.env.EMAIL,
+        to : i.email,
+        subject : 'Certify Email',
+        html : getHtml(i.name, i.product_name, i.price, i.time_stamp),
+      };
+
+      await transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+          return console.log(error);
+        }
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  // }
+  // check();
 });
--- models/Items.js
+++ models/Items.js
@@ -2,49 +2,33 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
-    product_name: String,
-    date: { type: Date, default:new Date() },
-    time_stamp: Date,
-    photos: {
-        type: Array,
-        default: []
-    },
-    details: String,
-    base_price: Number,
-    sold: {type: Boolean, default: false},
-    expired:{type: Boolean, default: false},
-    sent: {type: Boolean, default: false},
-    location: String, 
-    
-    // bid section
-    seller:{
-        name: String,
-        _id: String,
-    }, 
-    current_bid:{
-        amount: Number,
-        name: {
-            type: String, 
-            default: "-none-"
-        },
-        user_id:{
-            type: String,
-            default: "-none"
-        }
-    },
-    winner:{
-        name: {
-            type: String,
-            default: "-none-"
-        },
-        _id : {
-            type: String,
-            default: "-none-"
-        },
-        amount: Number
-    }
-});
+  product_name : String,
+  date : {type : Date, default : new Date()},
+  time_stamp : Date,
+  photos : {type : Array, default : []},
+  details : String,
+  base_price : Number,
+  sold : {type : Boolean, default : false},
+  expired : {type : Boolean, default : false},
+  sent : {type : Boolean, default : false},
+  location : String,
 
+  // bid section
+  seller : {
+    name : String,
+    _id : String,
+  },
+  current_bid : {
+    amount : Number,
+    name : {type : String, default : "-none-"},
+    user_id : {type : String, default : "-none"}
+  },
+  winner : {
+    name : {type : String, default : "-none-"},
+    _id : {type : String, default : "-none-"},
+    amount : Number
+  }
+});
 
 let Item = mongoose.model('Items', ItemSchema);
 
